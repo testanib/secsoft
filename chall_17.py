f5d935f7e63c2b0a40ceafa57481054198a0bfc9
testanib <45275431+testanib@users.noreply.github.com>
2021-04-12 21:27:24 -0400

    Create chall_17.py

---
+++ chall_17.py
@@ -0,0 +1,9 @@
+from pwn import *
+binary=ELF("./chall_17")
+p=process("./chall_17")
+from ctypes import *
+#c_lib=ctypes.LoadLibrary('libc.so.6')
+c_lib=cdll.LoadLibrary('libc.so.6')
+c_lib.srand(c_lib.time(None))
+p.sendline(str(c_lib.rand()))
+print(p.recvuntil('\n'))
